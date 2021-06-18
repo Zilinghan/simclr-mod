6b8f19947ff2093105e5e8c877ba0d18f9124c93
Zilinghan <Zilinghan.18@intl.zju.edu.cn>
2021-06-18 16:46:36 +0800

    another_init

---
+++ models/resnet_simclr.py
@@ -0,0 +1,36 @@
+# Comments on `resnet_simclr`
+# This file defines the class `RestNetSimCLR`, which is the deep neural network used for
+# the contrastive learning.
+# To use the network, simple call `ResNetSimCLR_ins.forword(img)`
+
+import torch.nn as nn
+import torchvision.models as models
+
+from exceptions.exceptions import InvalidBackboneError
+
+
+# nn.Module is the base class for all neural networks
+class ResNetSimCLR(nn.Module):
+
+    def __init__(self, base_model, out_dim):
+        super(ResNetSimCLR, self).__init__()
+        self.resnet_dict = {"resnet18": models.resnet18(pretrained=False, num_classes=out_dim),
+                            "resnet50": models.resnet50(pretrained=False, num_classes=out_dim)}
+        # backbone is just the main neural network - either Resnet-18 or Resnet-50
+        self.backbone = self._get_basemodel(base_model)
+        dim_mlp = self.backbone.fc.in_features
+
+        # add mlp projection head
+        self.backbone.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.backbone.fc)
+
+    def _get_basemodel(self, model_name):
+        try:
+            model = self.resnet_dict[model_name]
+        except KeyError:
+            raise InvalidBackboneError(
+                "Invalid backbone architecture. Check the config file and pass one of: resnet18 or resnet50")
+        else:
+            return model
+
+    def forward(self, x):
+        return self.backbone(x)
